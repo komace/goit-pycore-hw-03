7d7a328753869aa9995700351755efffefe8b3ca
komace <30932472+komace@users.noreply.github.com>
2024-07-21 23:58:42 +0200

    виконані 4 домашні завдання

--- Homework4.py
+++ Homework4.py
@@ -5,7 +5,7 @@ def get_upcoming_birthdays(users: list[dict]) -> list[dict]:
     today = datetime.today().date()
     # обчислюємо дату яка буде через 7 днів за допомогою класу timedelta
     next_week = today + timedelta(days=7)
-    # задаємо зміну пуст
+    # задаємо зміну пусту
     upcoming_birthdays = []
     
     # використовуємо цикл для перевірки днів народження
